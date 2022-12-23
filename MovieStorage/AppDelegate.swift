705a3961dda1ebeb46313b54642cb42b2b988719
wonhui-kim <0512tini@gmail.com>
2022-12-23 17:16:53 +0900

    [setting] iOS 13.0 이하 버전 대응을 위한 AppDelegate 수정 (#1)

--- MovieStorage/AppDelegate.swift
+++ MovieStorage/AppDelegate.swift
@@ -11,10 +11,18 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if #available(iOS 13.0, *) {
+            
+        } else {
+            window = UIWindow()
+            window?.rootViewController = ViewController()
+            window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
