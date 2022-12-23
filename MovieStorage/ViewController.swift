702ca6ab931bc626b5d2c1e7b6994a3f53f30083
wonhui-kim <0512tini@gmail.com>
2022-12-23 17:05:54 +0900

    Initial Commit

---
+++ MovieStorage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieStorage
+//
+//  Created by kim-wonhui on 2022/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
