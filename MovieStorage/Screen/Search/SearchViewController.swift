bf77e0088c4e579df747d6301f04c95ddd124202
wonhui-kim <0512tini@gmail.com>
2022-12-23 22:58:33 +0900

    [feat] 검색창 클릭 시 키보드 엔터키 이름 "검색"으로 설정 (#11)

--- MovieStorage/Screen/Search/SearchViewController.swift
+++ MovieStorage/Screen/Search/SearchViewController.swift
@@ -11,6 +11,7 @@ class SearchViewController: UIViewController {
     
     private let searchController: UISearchController = {
         let controller = UISearchController(searchResultsController: SearchResultsViewController())
+        controller.searchBar.returnKeyType = .search
         return controller
     }()
 
